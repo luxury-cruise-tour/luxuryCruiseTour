7408fa6e4c49adcf34255bcfd46ba6b57b3c776b
nicole-nam <eunjungnam@gmail.com>
2023-05-21 22:15:16 -0400

    Added initial error component and its styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navigation from './components/Navigation';
+import Error from './components/Error';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <Error />
     </div>
   );
 }
