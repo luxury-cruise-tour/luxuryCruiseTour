110e5c57033cda5185e825631bb043f789178036
nicole-nam <eunjungnam@gmail.com>
2023-05-24 13:36:55 -0400

    Added nav styling

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,16 +7,21 @@ const Navigation = () =>{
     const [toursLeft, setToursLeft] = useState(3);
 
     return(
-        <nav className='wrapper'>
+        <nav className="navigation wrapper">
+            <div className="logo">
+                <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48"><g fill="none" fill-rule="evenodd"><circle cx="24" cy="24" r="24" fill="#FFF"/><path fill="#0B0D17" d="M24 0c0 16-8 24-24 24 15.718.114 23.718 8.114 24 24 0-16 8-24 24-24-16 0-24-8-24-24z"/></g></svg>
+            </div>
             <ul>
-                <li>
+                <li className="links">
                     <Link to="/">Home</Link>
                 </li>
-                <li>
+                <li className="links">
                     <Link to="/tours">Tours</Link>
                 </li>
+                <li>
+                    <p>You have <span>{toursLeft}</span> virtual tours left today!</p>
+                </li>
             </ul>
-            <p>You have <span>{toursLeft}</span> virtual tours left today!</p>
         </nav>
     )
 
