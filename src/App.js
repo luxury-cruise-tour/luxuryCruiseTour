ee45d6ba4b252d5f56024874a61aeb128e73902f
Osman Omar <o.ali.omar@gmail.com>
2023-05-25 11:25:39 -0400

    added Dates component to App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Locations from './components/Locations';
 import Home from './components/Home';
 import Navigation from './components/Navigation';
 import Error from './components/Error';
+import Dates from './components/Dates';
 
 
 function App() {
   return (
     <div className="App">
-      <Navigation />  
+      <Navigation />
+      <Dates />
       <Home />
       <Locations />
       <Error />
