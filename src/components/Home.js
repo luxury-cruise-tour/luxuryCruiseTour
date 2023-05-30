ca8f2159db792298c4b6c16e80a7c70c43dccfde
Mohammed Shaikh <shaikh.adnan@hotmail.com>
2023-05-29 22:13:58 -0400

    Changed components for best practice and css clean up

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,21 +3,22 @@ import companyLogo from "./../assets/companyLogo.png";
 const Home = () => {
 
   return (
-    <div className="Home padding-top">
-      <div className="wrapper">
-        <header>
-            <div className="Hero">
-                <h1>YBS Galactic Tours</h1>
-                <img src={companyLogo} alt="Company Logo"/>
-            </div>
-        </header>
+    <header>
+      <div className="Home paddingTop">
+        <div className="wrapper">
+
+          <div className="Hero">
+            <h1>YBS Galactic Tours</h1>
+            <img src={companyLogo} alt="Company Logo" />
+          </div>
           <div className="aboutContainer">
             <div className="about">
-                <p>Let's face it: if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well, sit back and relax because we'll give you a truly out-of-this-world experience!</p>
+              <p>Let's face it: if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well, sit back and relax because we'll give you a truly out-of-this-world experience!</p>
             </div>
           </div>
         </div>
       </div>
+    </header>
   );
 }
 
